0b023dc054f2b9632da9c6087154348e207b041b
juan18506 <juan185067@gmail.com>
2023-10-18 00:48:16 -0300

    adapter pattern

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
-
 // const { emailTemplate } = require('./js-foundation/01-template');
 // require('./js-foundation/02-destructuring');
-const { getUserById } = require('./js-foundation/03-callbacks');
+// const { getUserById } = require('./js-foundation/03-callbacks');
+// require('./js-foundation/04-arrow-functions');
+require('./js-foundation/05-factory');
 
-const id = 1;
+// const id = 1;
 
-getUserById( id, ( error, user ) => {
-  if ( error ) {
-    throw new Error( error );
-  }
+// getUserById( id, ( error, user ) => {
+//   if ( error ) {
+//     throw new Error( error );
+//   }
 
-  console.log({ user });
-});
+//   console.log({ user });
+// });

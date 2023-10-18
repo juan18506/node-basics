6591cab98ebeb4bd3b797a94410da306059c9c70
juan18506 <juan185067@gmail.com>
2023-10-18 20:40:49 -0300

    changed fetch API to axios

--- src/plugins/http-client.plugin.js
+++ src/plugins/http-client.plugin.js
@@ -1,9 +1,9 @@
+const axios = require('axios');
 
 const httpClientPlugin = {
 
   get: async ( url ) => {
-    const response = await fetch( url );
-    const data = await response.json();
+    const { data } = await axios.get( url );
     return data;
   },
 
